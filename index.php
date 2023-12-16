975ed26827051e18d069f5c5afec34a597b2898a
luizdevfelipe <lipefavaro2005@gmail.com>
2023-12-16 19:38:29 -0300

    páginas de login e registro unificadas

--- index.php
+++ index.php
@@ -2,87 +2,110 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Login</title>
-    <link rel="stylesheet" href="estilos/login.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        body,
-        html {
-            background-image: url("images/caixas-pc.jpg");
-            background-size: cover;
-            background-repeat: no-repeat;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="estilos/inicial.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
 
 <body>
-    <?php
-    function validar($dado)
-    {
-        $dado = trim($dado);
-        $dado = stripslashes($dado);
-        $dado = htmlspecialchars($dado);
-        return $dado;
-    }
+  
 
-    $nome = $senha = $a = $erro = '';
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = validar($_POST["user"]);
-        $senha = validar($_POST["senha"]);
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "marketplace";
+  <header>
+    <menu class="pt-1">
+      <p>MarketPlace</p>
+      <form action="" method="post">
+        <input type="text" name="pesquisa" id="ipesquisa" placeholder="Pesquise">
+        <input type="submit" value="Buscar">
+      </form>
+      <a href="login.php"><i class="bi bi-person-circle"></i></a>
+    </menu>
+  </header>
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+  <main>
+    <div id="principal">
+      <section id="texto"><strong>
+          <mark>Tudo pra você</mark> <br><mark> de P a Q
+        </strong></mark></section>
+    </div>
 
-        if ($conn->connect_error) {
-            die($conn->connect_error);
-        }
+    <div class="container-fluid bg-secondary" style="width: 100%; height: 70px;">
 
-        $sql = "SELECT * FROM usuarios WHERE nome = '" . $nome . "' AND senha = '" . $senha . "' ";
-        $resultado = $conn->query($sql);
+    </div>
 
-        if ($resultado->num_rows > 0) {
-            $a = "window.location.href = 'http://localhost/marketplace/inicial.php'";
-        } else {
-            $erro = "Usuário ou senha inválidos";
-        }
+    <div class="container">
+      <div class="row">
 
-        $conn->close();
-    }
-    ?>
-
-    <fieldset class="centralizado border border-dark rounded text-center w350">
-        <legend class="mt-1 display-6">Login</legend>
-        <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post" autocomplete="on">
-            <label for="iuser" class="mt-1 lead">Nome de Usuário:</label><br>
-            <input type="text" class="p-1" name="user" id="iuser" autocomplete="username" required minlength="4" maxlength="50"><br>
-
-            <label for="isenha" class="mt-1 lead">Insira sua Senha:</label><br>
-            <input type="password" class="p-1" name="senha" id="isenha" autocomplete="current-password" minlength="8" required maxlength="15"><br>
-
-            <input class="btn btn-success my-2" type="submit" value="Entrar">
-        </form>
-
-        <div class="erro bg-white text-danger rounded m-2 fs-5">
-            <?= $erro ?>
+        <div class="card col-3 mt-3 d-block m-auto" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Bicicleta</h5>
+            <p class="card-text">Bicicleta usada aro 29 freio a disco 24 marchas</p>
+            <p class="card-text">R$ 600</p>
+            <a href="#" class="btn btn-primary">Ver Produto</a>
+          </div>
         </div>
-    </fieldset>
 
-    <div class="text-center lead">
-        <p>Não tem cadastro? <br><a href="registro.php" class="text-success text-decoration-none">Registre-se</a> agora</p>
+        <div class="card col-3 mt-3 d-block m-auto" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+        <div class="card col-3 mt-3 d-block m-auto" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+        <div class="card col-3 mt-3 d-block m-auto" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+
+        <div class="card col-3 mt-3 d-block m-auto" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+
+      </div>
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </main>
 
-    <script>
-        <?=$a?>
-    </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+  <script>
+    function Perfil(){
+      if (<?=$nome?> == ''){
+        window.location.href = 'http://localhost/marketplace/login.php'
+      } else {  
+        window.location.href = 'http://localhost/marketplace/perfil.php'
+      }
+    }
+  </script>
+
 </body>
 
 </html>
---
+++ login.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de Login</title>
+    <link rel="stylesheet" href="estilos/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body,
+        html {
+            background-image: url("images/caixas-pc.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    function validar($dado)
+    {
+        $dado = trim($dado);
+        $dado = stripslashes($dado);
+        $dado = htmlspecialchars($dado);
+        return $dado;
+    }
+
+    $user = $senha = $senha1 = $senha2 = $erro = $cor = $p = '';
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "marketplace";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die($conn->connect_error);
+        }
+
+        if (count($_POST) == 2) {
+            $nome = validar($_POST["user"]);
+            $senha = validar($_POST["senha"]);
+
+            $sql = "SELECT * FROM usuarios WHERE nome = '" . $nome . "' AND senha = '" . $senha . "' ";
+            $resultado = $conn->query($sql);
+
+            if ($resultado->num_rows > 0) {                
+                $p = "window.location.href = 'http://localhost/marketplace/perfil.php'";
+            } else {
+                $erro = "Usuário ou senha inválidos";
+                $cor = "text-danger";
+            }
+        } else {
+            $user = validar($_POST["user"]);
+            $senha1 = validar($_POST["senha1"]);
+            $senha2 = validar($_POST["senha2"]);
+
+            if ($senha1 == $senha2) {
+                $senha = $senha1;
+
+                $sql = "INSERT INTO usuarios (nome, senha) VALUES ('" . $user . "', '" . $senha . "')";
+
+                if ($conn->query($sql)) {
+                    $erro = "Cadastro Realizado!";
+                    $cor = "text-success";
+                }
+            } else {
+                $erro = 'Senhas Diferentes!';
+                $cor = "text-danger";
+            }
+        }
+        $conn->close();
+    }
+    ?>
+
+    <fieldset class="centralizado border border-dark rounded text-center w350">
+        <legend class="mt-1 display-6" id="legenda">Login</legend>
+        <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post" autocomplete="on" id="form">
+            <label for="iuser" class="mt-1 lead">Nome de Usuário:</label><br>
+            <input type="text" class="p-1" name="user" id="iuser" autocomplete="username" required minlength="4" maxlength="50"><br>
+
+            <label for="isenha" class="mt-1 lead">Insira sua Senha:</label><br>
+            <input type="password" class="p-1" name="senha" id="isenha" autocomplete="current-password" minlength="8" required maxlength="15"><br>
+
+            <input class="btn btn-success my-2" type="submit" value="Entrar">
+        </form>
+
+        <div id="diverro" class="erro bg-white <?= $cor ?> rounded m-2 fs-5">
+            <?= $erro ?>
+        </div>
+    </fieldset>
+
+    <div class="text-center lead" id="registro">
+        <p>Não tem cadastro? <br><span class="text-success text-decoration-none" onclick="Registrar()">Registre-se</span> agora</p>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <script>
+        <?= $p ?>
+
+        function Registrar() {
+            document.getElementById('diverro').innerHTML = ''
+            form = document.querySelector('form#form')
+            legend = document.querySelector('legend#legenda')
+            document.getElementById('registro').style.display = 'none'
+
+            legend.innerHTML = 'Registre-se'
+            form.innerHTML = "<label for='iuser' class='mt-1 lead'>Nome de Usuário:</label><br><input type='text' class='p-1' name='user' id='iuser' autocomplete='username' required minlength='4' maxlength='50'><br><label for='isenha1' class='mt-1 lead'>Insira sua Senha:</label><br><input type='password' class='p-1' name='senha1' id='isenha1' autocomplete='current-password' minlength='8' required maxlength='15'><br><label for='isenha2' class='mt-1 lead'>Repita a Senha:</label><br><input type='password' class='p-1' name='senha2' id='isenha2' autocomplete='current-password' required minlength='8' maxlength='15'><br><input class='btn btn-success my-2' type='submit' value='Entrar'>"
+        }
+    </script>
+</body>
+
+</html>
