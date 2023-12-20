b7c6c23386c481d996c10010ef3a47cf69f5d61f
luizdevfelipe <lipefavaro2005@gmail.com>
2023-12-20 14:41:03 -0300

    usuário consegue alterar dados

--- user/perfil.php
+++ user/perfil.php
@@ -19,106 +19,111 @@
                 <input type="text" name="pesquisa" id="ipesquisa" placeholder="Pesquise">
                 <input type="submit" value="Buscar">
             </form>
-            <a href="carrinho.php"><i class="bi bi-cart3 ms-1"></i></a>
+            <a href="carrinho.php"><i class="bi bi-cart3 ms-1 fs-3"></i></a>
         </menu>
     </header>
 
     <main>
-        <?php 
-            session_start();
-            $id = $_SESSION['id'];           
+        <?php
+        session_start();
+        $id = $_SESSION['id'];
 
-            function validar($dado){
-                $dado = trim($dado);
-                $dado = stripslashes($dado);
-                $dado = htmlspecialchars($dado);
-                return $dado;
+        function validar($dado)
+        {
+            $dado = trim($dado);
+            $dado = stripslashes($dado);
+            $dado = htmlspecialchars($dado);
+            return $dado;
+        }
+
+        $nome = $sobrenome = $estado = $cidade = $foto = $p = '';
+
+        $servername = 'localhost';
+        $username = 'root';
+        $password = '';
+        $dbname = 'marketplace';
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die($conn->connect_error);
+        }
+
+        $sql = "SELECT * FROM usuarios WHERE id = '" . $id . "'";
+        $result = $conn->query($sql);
+        $vet = $result->fetch_assoc();
+
+        if ($vet["foto"] != ''){
+            $local = $vet["foto"];
+        } else { $local = ''; }
+
+        if ($vet["sobrenome"] != '') {
+            $nome = $vet["nome"];
+            $sobrenome = $vet["sobrenome"];
+            $estado = $vet["estado"];
+            $cidade = $vet["cidade"];
+        } else {
+            $p = "cadastrado()";
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_FILES["foto"])) {                
+                $foto = $_FILES["foto"];
+
+                if($foto["error"]){
+                    die("Falha ao enviar ao arquivo");
+                }
+
+                if($foto["size"] > 2097152){
+                    die("Arquivo muito grande! Máximo de 2 MegaBytes");                                       
+                }
+
+                $pasta = "fotos/";
+                $nomeOriginal = $foto["name"];
+                $nomeCodificado = uniqid();
+                $extensao = strtolower(pathinfo($nomeOriginal, PATHINFO_EXTENSION));
+
+                $path = $pasta . $nomeCodificado . '.' . $extensao;
+
+                if ($extensao != 'jpg' && $extensao != 'png'){
+                    die("Arquivo não suportado");
+                }
+
+                $moveu = move_uploaded_file($foto["tmp_name"], $path);
+
+                if($moveu){
+                    $sql = "UPDATE usuarios SET foto ='$path' WHERE id = '$id'";
+                    $conn->query($sql);
+                } else {
+                    echo "Falha ao salvar o arquivo";
+                }
             }
 
-            $nome = $sobrenome = $estado = $cidade = $foto = '';
-            if($_SERVER["REQUEST_METHOD"] == "POST"){
-                $servername = 'localhost';
-                $username = 'root';
-                $password = '';
-                $dbname = 'marketplace';
-
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                if ($conn->connect_error){
-                    die($conn->connect_error);
-                }
-                //if valores acima já cadastrados, exiba-os
-
-                //else cadastre os valores
+            if (count($_POST) > 0) {
                 $nome = validar($_POST["nome"]);
                 $sobrenome = validar($_POST["sobrenome"]);
                 $estado = validar($_POST["estado"]);
                 $cidade = validar($_POST["cidade"]);
 
-                $sql = "INSERT INTO  usuarios (nome, sobrenome, estado, cidade) VALUES ('".$nome."', '".$sobrenome."', '".$estado."', '".$cidade."')";
+                $sql = "UPDATE usuarios SET nome = '" . $nome . "', sobrenome = '" . $sobrenome . "', estado = '" . $estado . "', cidade = '" . $cidade . "' WHERE id = '" . $id . "' ";
 
-                if($conn->query($sql) === TRUE){
-                    echo "Cadastrado";
-                }
-
-
-                $conn->close();
+                $resultado = $conn->query($sql);
             }
-            
+        }
+
+        $conn->close();
         ?>
         <div class="container mt-2">
             <div class="row">
-                <div class="col-12 col-md-4 text-center text-md-start">
-                    <img src="" alt="" class="align-center" style="width: 200px; height:200px"><br>
-                    <label for="ifoto" class="border border-dark rounded p-1 mt-1 text-center" style="width: 200px;">Clique e envie a Imagem</label><br>
-                   
+                <div class="col-12 col-md-4 text-center text-md-start" id="foto">
+                    <img src="<?=$local?>" alt="" class="align-center" style="width: 200px; height:200px"><br>
+
                 </div>
-                <div class="col-12 col-md-8 text-center text-md-start pt-3 mt-3 mt-md-0 border border-dark rounded">
-                    <form action="<?=htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post" autocomplete="on">
-                    <input type="file" name="foto" id="ifoto" style="display: none;">
 
-                        <label for="inome" class="mb-2" style="margin-right: 40px;">Nome: *</label>
-                        <input type="text" name="nome" id="inome" maxlength="20" minlength="4" required><br>
+                <div class="col-12 col-md-8 text-center text-md-start pt-3 mt-3 mt-md-0 border border-dark rounded" id="infouser">
+                    Bem vindo <?php echo " $nome $sobrenome"; ?>, você mora em <?php echo " $cidade $estado"; ?>
+                    <br>
+                    <button class="mt-3" onclick="cadastrado()">Alterar Dados</button>
 
-                        <label for="isobrenome" class="mb-2">Sobrenome: *</label>
-                        <input type="text" name="sobrenome" id="isobrenome" maxlength="20" minlength="4" required><br>
-
-                        <label for="iestado" class="mb-2" style="margin-right: 33px;">Estado: *</label>
-                        <select name="estado" id="iestado" class="p-1" style="width: 199px;" required>                            
-                            <option value="AC">Acre</option>
-                            <option value="AL">Alagoas</option>
-                            <option value="AP">Amapá</option>
-                            <option value="AM">Amazonas</option>
-                            <option value="BA">Bahia</option>
-                            <option value="CE">Ceará</option>
-                            <option value="DF">Distrito Federal</option>
-                            <option value="ES">Espírito Santo</option>
-                            <option value="GO">Goiás</option>
-                            <option value="MA">Maranhão</option>
-                            <option value="MT">Mato Grosso</option>
-                            <option value="MS">Mato Grosso do Sul</option>
-                            <option value="MG">Minas Gerais</option>
-                            <option value="PA">Pará</option>
-                            <option value="PB">Paraíba</option>
-                            <option value="PR">Paraná</option>
-                            <option value="PE">Pernambuco</option>
-                            <option value="PI">Piauí</option>
-                            <option value="RJ">Rio de Janeiro</option>
-                            <option value="RN">Rio Grande do Norte</option>
-                            <option value="RS">Rio Grande do Sul</option>
-                            <option value="RO">Rondônia</option>
-                            <option value="RR">Roraima</option>
-                            <option value="SC">Santa Catarina</option>
-                            <option value="SP">São Paulo</option>
-                            <option value="SE">Sergipe</option>
-                            <option value="TO">Tocantins</option>
-                        </select> <br>
-
-                        <label for="icidade" style="margin-right: 32px;">Cidade: *</label>
-                        <input type="text" name="cidade" id="icidade" maxlength="20" minlength="4" required><br>
-
-                        <input type="submit" value="Enviar" class="mt-2 p-1">
-
-                    </form>
                 </div>
             </div>
         </div>
@@ -132,6 +137,21 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <script>
+        <?= $p ?>
+
+        function cadastrado() {
+            container = document.getElementById('infouser')
+            foto = document.getElementById('foto')
+            formulario = "<form action='<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>' method='post' autocomplete='on'><label for='inome' class='mb-2' style='margin-right: 41px;'>Nome:*</label><input type='text' name='nome' id='inome' maxlength='20' minlength='4' required><br><label for='isobrenome' class='mb-2' style='margin-right: 1px;'>Sobrenome:*</label><input type='text' name='sobrenome' id='isobrenome' maxlength='20' minlength='4' required><br><label for='iestado' class='mb-2' style='margin-right: 34px;'>Estado:*</label><select name='estado' id='iestado' class='p-1' style='width: 199px;' required><option value='AC'>Acre</option><option value='AL'>Alagoas</option><option value='AP'>Amapá</option><option value='AM'>Amazonas</option><option value='BA'>Bahia</option><option value='CE'>Ceará</option><option value='DF'>Distrito Federal</option><option value='ES'>Espírito Santo</option><option value='GO'>Goiás</option><option value='MA'>Maranhão</option><option value='MT'>Mato Grosso</option><option value='MS'>Mato Grosso do Sul</option><option value='MG'>Minas Gerais</option><option value='PA'>Pará</option><option value='PB'>Paraíba</option><option value='PR'>Paraná</option><option value='PE'>Pernambuco</option><option value='PI'>Piauí</option><option value='RJ'>Rio de Janeiro</option><option value='RN'>Rio Grande do Norte</option><option value='RS'>Rio Grande do Sul</option><option value='RO'>Rondônia</option><option value='RR'>Roraima</option><option value='SC'>Santa Catarina</option><option value='SP'>São Paulo</option><option value='SE'>Sergipe</option><option value='TO'>Tocantins</option></select> <br><label for='icidade' style='margin-right: 33px;'>Cidade:*</label><input type='text' name='cidade' id='icidade' maxlength='20' minlength='4' required><br><input type='submit' value='Enviar' class='mt-2 p-1'></form>"
+
+            botao = "<form action='<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>' enctype='multipart/form-data' method='post'><label for='ifoto' class='border border-dark rounded p-1 mt-1 text-center' style='width: 200px; cursor:pointer;'>Clique e envie a Imagem</label><input type='file' name='foto' id='ifoto' style='display: none;'> <input type='submit' class='border border-dark rounded p-1 mt-1 text-center' value='Salvar Imagem'></form>"
+
+            container.innerHTML = formulario
+            foto.innerHTML += botao
+        }
+    </script>
 </body>
 
 </html>
