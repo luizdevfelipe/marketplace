f1a20cd85f0b6956a6489e075049610fa2ba33e1
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-10-08 23:25:42 -0300

    Adicionando novos campos em $fillable
    Permitindo que os campos: lastname, state e city possam ser atribuídos em massa, através do método create() utilizado em UserService

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,9 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
+        'lastname',
+        'state',
+        'city',
     ];
 
     /**
