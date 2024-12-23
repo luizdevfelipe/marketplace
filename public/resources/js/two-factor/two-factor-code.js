52654cc08329a1a269c7894a4079f7fe32e6d38d
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-12-23 11:23:55 -0300

    gerando novos códigos de recuperação 2FA
    Através de uma requisição ajax para /user/two-factor-recovery-codes são gerados novos códigos de recuperação da autenticação em dois fatores

--- public/resources/js/two-factor/two-factor-code.js
+++ public/resources/js/two-factor/two-factor-code.js
@@ -1,24 +1,41 @@
 window.addEventListener('DOMContentLoaded', function () {
     document.querySelector('button#getCodes').addEventListener('click', function (event) {
-        fetch('/user/two-factor-recovery-codes', {
-            method: 'GET'
-        }).then(response => {
-            const contentType = response.headers.get("content-type");
-        
-            if (response.redirected) {
-                window.location.href = response.url;
-            } else if (response.ok && contentType && contentType.includes("application/json")) {
-                return response.json();
-            }
-        })
-        .then(function (data) {
-            data.forEach(code => {
-                var li = document.createElement('li')
-                li.classList.add('list-group-item')
-                li.innerHTML = code
-                document.querySelector('ul#codes').appendChild(li)
-            });
-            event.target.remove()
-        })
+        show2FACodes()
     })
+
+    document.querySelector('button#newCodes').addEventListener('click', function (event) {
+        fetch('/user/two-factor-recovery-codes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            },
+        })
+        event.target.remove()
+        show2FACodes()
+    });
 })
+
+function show2FACodes() {
+    fetch('/user/two-factor-recovery-codes', {
+        method: 'GET'
+    }).then(response => {
+        const contentType = response.headers.get("content-type");
+
+        if (response.redirected) {
+            window.location.href = response.url;
+        } else if (response.ok && contentType && contentType.includes("application/json")) {
+            return response.json();
+        }
+    }).then(function (data) {
+        list = document.querySelector('ul#codes')
+        list.innerHTML = ''
+        data.forEach(code => {
+            var li = document.createElement('li')
+            li.classList.add('list-group-item')
+            li.innerHTML = code
+            list.appendChild(li)
+        });
+        document.querySelector('button#getCodes').remove()
+    })
+}
--- resources/views/user/manage2fa.blade.php
+++ resources/views/user/manage2fa.blade.php
@@ -41,12 +41,13 @@
 
         <ul class="list-group m-2" style="width: 350px;" id='codes'></ul>
 
-        @if (!session()->has('auth.password_confirmed_at') || 
+        @if (!session()->has('auth.password_confirmed_at') ||
         time() - session('auth.password_confirmed_at') > config('auth.password_timeout', 10800))
 
         <a href="http://localhost/confirm-password" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover fs-4">Confirme a senha para ver seus códigos de autenticação</a>
         @else
         <button id="getCodes" class="btn btn-primary">Visualizar códigos de recuperação</button>
+        <button id="newCodes" class="btn btn-warning">Gerar novos códigos</button>
         @endif
 
         @else
