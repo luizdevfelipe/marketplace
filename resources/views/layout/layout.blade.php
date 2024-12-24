b8f64dc9ab4f428ac6ca83bb52d23264d2fe80ea
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-12-24 14:15:30 -0300

    alteração na rota de pesquisa

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -12,16 +12,17 @@
   <link rel="stylesheet" href="/resources/css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+  <script src="/resources/js/layout.js"></script>
 </head>
 
 <body>
   <header>
     <menu class="pt-1">
       <p class="p-0 m-0"><a href="/" class="text-decoration-none fs-5">MarketPlace</a></p>
-      <form action="/produto/search" method="get" autocomplete="on">
+      <div class="menuForm">
         <input type="text" name="produto" id="ipesquisa" placeholder="Pesquise" required>
-        <input type="submit" value="Buscar">
-      </form>
+        <button class="btn bg-primary text-white" id="buscarProduto">Buscar</button>
+      </div>
       <a href="/perfil"><i class="bi bi-person-circle ms-1 fs-3"></i></a>
       <a href="/carrinho"><i class="bi bi-cart3 ms-1 fs-3"></i></a>
     </menu>
