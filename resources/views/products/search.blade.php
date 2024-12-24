e0440599dc105d07477bebaa695cb831a7cca92f
luizdevfelipe <lipefavaro2005@gmail.com>
2024-12-24 11:17:32 -0300

    view adequado aos padrões do Blade

--- resources/views/products/search.blade.php
+++ resources/views/products/search.blade.php
@@ -4,25 +4,24 @@
 
 @section('body')
 <main>
-    <div class="container text-center">
-        <?php if (!empty($results) && $results !== '') : ?>
-            <?php foreach ($results as $row) : ?>
-
-                <div class='card d-block m-auto mt-3 p-2' style='width: 18rem; height:450px'><img src='<?= asset('storage/' . $row['product_picture']); ?>' class='card-img-top rounded' style='height: 220px' alt='...'>
-                    <div class='card-body'>
-                        <h5 class='card-title'><?= $row['name'] ?></h5>
-                        <p class='card-text'><?= $row['description'] ?></p>
-                        <p class='card-text'>R$<?= $row['price'] ?></p><a href="/produto?id=<?= $row['id'] ?>" class='btn btn-primary'>Ver Produto</a>
+    <div class="container p-2">
+        <div class="row row-cols-md-3 row-cols-lg-4">
+            @if (!empty($results) && $results !== '')
+                @foreach ($results as $row)
+                    <div class='card p-2' style='height:450px'><img src="{{ asset('storage/' . $row['product_picture']) }}" class='card-img-top rounded' style='height: 220px' alt='...'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>{{ $row['name'] }}</h5>
+                            <p class='card-text'>{{ $row['description'] }}</p>
+                            <p class='card-text'>R${{ $row['price'] }}</p><a href="/produto/{{ $row['id'] }}" class='btn btn-primary'>Ver Produto</a>
+                        </div>
                     </div>
+                @endforeach
+            @else
+                <div class="container border border-dark rounded mt-5 d-block m-auto">
+                    <h1 class="text-center p-5"> Produto não encontrado &#x1F641;<br>Tente utilizar palavras mais curtas como: pipoca, prato ou panela.</h1>
                 </div>
-
-            <?php endforeach; ?>
-        <?php else : ?>
-            <div class="container border border-dark rounded mt-5 d-block m-auto">
-                <h1 class="text-center p-5"> Produto não encontrado &#x1F641;<br>Tente utilizar palavras mais curtas como: pipoca, prato ou panela.</h1>
-            </div>
-
-        <?php endif; ?>
+            @endif
+        </div>
     </div>
 </main>
 @endsection
