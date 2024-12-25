f4290243a0123921cef469e520dbd1bad06c0669
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2024-12-25 20:50:51 -0300

    melhora na visualização da paginação
    Melhorando a visualização de itens no sistema de paginação de pesquisa por produtos

--- resources/views/products/search.blade.php
+++ resources/views/products/search.blade.php
@@ -5,25 +5,27 @@
 @section('body')
 <main>
     <div class="container p-2">
-        <div class="row row-cols-md-3 row-cols-lg-4">
-            @if (!empty($results) && $results !== '')
-                @foreach ($results as $row)
-                    <div class='card p-2' style='height:450px'><img src="{{ asset('storage/' . $row['product_picture']) }}" class='card-img-top rounded' style='height: 220px' alt='...'>
-                        <div class='card-body'>
-                            <h5 class='card-title'>{{ $row['name'] }}</h5>
-                            <p class='card-text'>{{ $row['description'] }}</p>
-                            <p class='card-text'>R${{ $row['price'] }}</p><a href="/produto/{{ $row['id'] }}" class='btn btn-primary'>Ver Produto</a>
-                        </div>
-                    </div>
-                @endforeach
-
-                {{ $results->links() }}
-            @else
-                <div class="container border border-dark rounded mt-5 d-block m-auto">
-                    <h1 class="text-center p-5"> Produto não encontrado &#x1F641;<br>Tente utilizar palavras mais curtas como: pipoca, prato ou panela.</h1>
+        @if ($results->count() > 0)
+        <div class="row row-cols-md-3 ">
+            @foreach ($results as $row)
+            <div class='card p-2' style='height:450px'><img src="{{ asset('storage/' . $row['product_picture']) }}" class='card-img-top rounded' style='height: 220px' alt='...'>
+                <div class='card-body'>
+                    <h5 class='card-title'>{{ $row['name'] }}</h5>
+                    <p class='card-text'>{{ $row['description'] }}</p>
+                    <p class='card-text'>R${{ $row['price'] }}</p><a href="/produto/{{ $row['id'] }}" class='btn btn-primary'>Ver Produto</a>
                 </div>
+            </div>
+            @endforeach
+            
+            @else
+            <div class="container border border-dark rounded mt-5 d-block m-auto">
+                <h1 class="text-center p-5"> Produto não encontrado &#x1F641;<br>Tente utilizar palavras mais curtas como: pipoca, prato ou panela.</h1>
+            </div>
             @endif
         </div>
     </div>
+    <div class="text-center">
+        {{ $results->links() }}
+    </div>    
 </main>
 @endsection
