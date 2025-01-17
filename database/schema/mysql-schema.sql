b5d563e6d732baadb9adf7939a695d4b60564451
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-01-17 19:57:51 -0300

    mudanças no banco de dados
    Alteração na chave que representa as compras feitas, para que sejam equivalentes aos valores gerados pelo Mercado Pago

--- database/schema/mysql-schema.sql
+++ database/schema/mysql-schema.sql
@@ -123,12 +123,12 @@ DROP TABLE IF EXISTS `purchases`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `purchases` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `purchase_id` varchar(255) NOT NULL,
   `user_id` bigint unsigned NOT NULL,
   `product_id` bigint unsigned NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `status` tinyint NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `sessions`;
