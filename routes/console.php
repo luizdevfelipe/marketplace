3ca70b2ed605129c777410bb7c8e452adf529a8e
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-01-20 15:59:17 -0300

    Scheduler para remover pagamentos pendentes
    Com a utilização do serviço Laravel Scheduler foi possível definir uma tarefa agendata, através de um comando Artisan, para remover pagamentos pendentes antigos

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Enums\Payment\PaymentStatusEnum;
+use App\Models\Purchase;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('purchases:delete', function() {
+    Purchase::where('created_at', '<=', now()->subDays(1))
+        ->where('status', PaymentStatusEnum::PENDING->value)
+        ->delete();
+})->purpose('Delete pending purchases older than 1 day')->daily();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
