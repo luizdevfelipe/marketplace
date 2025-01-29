46f6b77cebfb508080bd33438786a5f2e72ab39d
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-01-29 17:22:45 -0300

    arquivo movido para resources/

--- public/resources/js/layout.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('button#buscarProduto').addEventListener('click', function (event) {
-        event.preventDefault();
-        let produto = document.querySelector('input#ipesquisa').value;
-        if (produto) {
-            window.location.href = `http://localhost/produto/search/${produto}`;
-        }
-    });
-});
--- public/resources/js/two-factor/two-factor-challenge.js
+++
@@ -1,8 +0,0 @@
-const code = '<label for="irecovery_code" class="mt-1 lead">Insira um código de recuperação válido:</label><br><input type="text" class="p-1" name="recovery_code" id="irecovery_code" required>'
-
-window.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('button#recovery').addEventListener('click', function (event) {
-        document.querySelector('div#codeLabel').innerHTML = code
-        event.target.remove()
-    })
-})
