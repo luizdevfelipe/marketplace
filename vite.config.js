ba07d2492b3467264d61db0ebf4f8aa31eaafb36
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-01-29 17:31:47 -0300

    novas entradas e configurações
    Com essa atualização foram adicioanas entradas para os arquivos de JavaScript para que sejam carregados individualmente quando necessário, além de um apelido para o diretório /resources e uma configuração de servidor

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,28 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.scss', 
+                'resources/js/app.js',
+                'resources/js/layout.js',
+                'resources/js/profile/load-paginate.js',
+                'resources/js/cart/remove-product.js',
+                'resources/js/cart/quantitySetter.js',
+                'resources/js/cart/checkout-cart.js',
+                'resources/js/two-factor/two-factor-challenge.js',
+                'resources/js/two-factor/two-factor-code.js',
+            ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': '/resources',
+        },
+    },
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
